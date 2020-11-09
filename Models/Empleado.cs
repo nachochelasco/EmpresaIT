f3751c4223d7ac3fe3d8a6147147bf3ed42c2f85
nachochelasco <nchelasco@gmail.com>
2020-11-09 17:55:23 -0300

    add clase Empleado con sus atributos

---
+++ Models/Empleado.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace model
+{
+    public class Empleado
+    {   
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID {get;set;}
+
+        [Required]
+        public string NombreCompleto {get;set;}
+
+        [Required]
+        public string Email {get;set;}
+
+        [Required]
+        public string PuestoDeTrabajo {get;set;}
+
+        [Required]
+        public int Sueldo {get;set;}
+    }
+}
