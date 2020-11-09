9ef52f6e2f16b198e8134c50a57d320961a28deb
nachochelasco <nchelasco@gmail.com>
2020-11-09 19:22:22 -0300

    add services en startup

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,10 @@ namespace EmpresaIT
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+             services.AddControllersWithViews()
+                .AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
 
             services.AddDbContext<EmpleadosContext>(options =>
             options.UseSqlite(Configuration.GetConnectionString("EmpleadosContext")));
